d50dbfb8a1840c12a0203a9889046a4dfe9262bb
tomBialick <tb7150@vt.edu>
2019-07-03 11:19:53 -0400

    removed users route/references and updated package dependencies

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -15,6 +13,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
