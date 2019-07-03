a886195c05f81703722b5a4410af053010b60f42
tomBialick <tb7150@vt.edu>
2019-07-03 13:40:10 -0400

    adding connection framework for postgresql

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+let conf_data = require('../config.json');
+
+const { Pool, Client } = require('pg');
+const pool = new Pool({
+  user: conf_data["database"]["username"],
+  host: conf_data["database"]["host"],
+  database: conf_data["database"]["db"],
+  password: conf_data["database"]["password"],
+  port: conf_data["database"]["port"],
+});
+const client = new Client();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/..', '/public', 'index.html'));
