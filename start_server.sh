10d1a5321227358082c9b4694769fb7a0d8242cc
tomBialick <tb7150@vt.edu>
2019-07-10 13:37:46 -0400

    Updated start_server.sh to start postgresql as well as node

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 # Start the server with this script
 
-npm start
-echo "running?"
+# 1. For Ubuntu uncomment the next chunk and comment out 2 and 3
+# sudo service postgresql stop
+# echo "Waiting for postgreSQL to stop"
+# sleep 3s
+# echo "Starting Server and postgreSQL"
+# npm start && sudo service postgresql start
+
+# 2. For MacOS uncomment the next chunk and comment out 1 and 3
+# pg_ctl -D /usr/local/var/postgres stop
+# echo "Waiting for postgreSQL to stop"
+# sleep 3s
+# echo "Starting Server and postgreSQL"
+# npm start && pg_ctl -D /usr/local/var/postgres start
+
+# 3. For Windows uncomment the next chunk and comment out 2 and 3 (CHECK VERSION!!)
+pg_ctl -D /c/Program Files/PostgreSQL/11/data stop
+echo "Waiting for postgreSQL to stop"
+sleep 3s
+echo "Starting Server and postgreSQL"
+npm start && pg_ctl -D /c/Program Files/PostgreSQL/11/data start
+
+
+echo "If you can read this, something probably failed..."
