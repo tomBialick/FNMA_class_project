7425c3d25c3d7d9d514754368174db5cd4eb8ef0
tomBialick <tb7150@vt.edu>
2019-07-10 14:53:37 -0400

    Fixed Error including JSON, responses, etc

--- routes/index.js
+++ routes/index.js
@@ -28,18 +28,18 @@ router.get('/list', function(req, res, next) {
     res.status(200).send(results)
   }).catch(error => {
     console.log('ERROR:', error);
-    res.status(400).send({"Bad Request"});
+    res.status(400).send("Bad Request");
   })
 //  res.status(200);
 });
 
 router.get('/list/query', function(req, res, next) {
-  res.status(200).send({"Ok"});
+  res.status(200).send("Ok");
 });
 
 router.get('/houseform', function(req, res, next) {
 
-  res.status(200).send({"Ok"});;
+  res.status(200).send("Ok");;
 });
 
 router.post('/house', function(req, res, next) {
@@ -61,18 +61,18 @@ router.post('/house', function(req, res, next) {
     let county = req.body.county;
 
     db.query( 'INSERT INTO APPRAISAL (ID, PROPERTY_ADDRESS, CITY, STATE, ZIP_CODE, BORROWER, OWNER_OF_PUBLIC_RECORD, COUNTY) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [file_id, address, city, state, zipcode, borrower, owner_of_record, county]).then(results => {
-      res.status(200).send({"Appraisal form successully added"});;
+      res.status(200).send("Appraisal form successully added");
     }).catch(error => {
       console.log('ERROR:', error);
-      res.status(400).send({"Bad Request"});
+      res.status(400).send("Bad Request");
     })
   }).catch(error => {
     console.log('ERROR:', error);
-    res.status(500).send({"Issue Connecting to Database"});
+    res.status(500).send("Issue Connecting to Database");
   })
 });
 
 router.get('/metrics', function(req, res, next) {
-  res.status(200).send({"Ok"});;
+  res.status(200).send("Ok");
 });
 module.exports = router;
--- start_server.sh
+++ start_server.sh
@@ -1,26 +1,81 @@
 #!/bin/bash
 # Start the server with this script
 
+# Colors for text
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+
 # 1. For Ubuntu uncomment the next chunk and comment out 2 and 3
-# sudo service postgresql stop
+# echo ""
+# echo "==================================================================="
 # echo "Waiting for postgreSQL to stop"
+# echo "==================================================================="
+# echo ""
+# sudo service postgresql stop
 # sleep 3s
-# echo "Starting Server and postgreSQL"
-# npm start && sudo service postgresql start
+# echo ""
+# echo "==================================================================="
+# echo "Starting postgreSQL"
+# echo "==================================================================="
+# echo ""
+# sudo service postgresql start
+# sleep 3s
+# echo ""
+# echo "==================================================================="
+# echo "Starting Server"
+# echo "==================================================================="
+# echo ""
+# npm start
 
 # 2. For MacOS uncomment the next chunk and comment out 1 and 3
-# pg_ctl -D /usr/local/var/postgres stop
+# echo ""
+# echo "==================================================================="
 # echo "Waiting for postgreSQL to stop"
+# echo "==================================================================="
+# echo ""
+# pg_ctl -D /usr/local/var/postgres stop
 # sleep 3s
-# echo "Starting Server and postgreSQL"
-# npm start && pg_ctl -D /usr/local/var/postgres start
+# echo ""
+# echo "==================================================================="
+# echo "Starting postgreSQL"
+# echo "==================================================================="
+# echo ""
+# pg_ctl -D /usr/local/var/postgres start
+# sleep 3s
+# echo ""
+# echo "==================================================================="
+# echo "Starting Server"
+# echo "==================================================================="
+# echo ""
+# npm start
 
 # 3. For Windows uncomment the next chunk and comment out 2 and 3 (CHECK VERSION!!)
-pg_ctl -D /c/Program Files/PostgreSQL/11/data stop
-echo "Waiting for postgreSQL to stop"
+echo ""
+echo "==================================================================="
+echo "Waiting for postgreSQL to ${RED}stop${NC}"
+echo "==================================================================="
+echo ""
+pg_ctl -D /c/Program\ Files/PostgreSQL/11/data stop
 sleep 3s
-echo "Starting Server and postgreSQL"
-npm start && pg_ctl -D /c/Program Files/PostgreSQL/11/data start
+echo ""
+echo "==================================================================="
+echo "{$GREEN}Starting postgreSQL${NC}"
+echo "==================================================================="
+echo ""
+pg_ctl -D /c/Program\ Files/PostgreSQL/11/data start
+sleep 3s
+echo ""
+echo "==================================================================="
+echo "{$GREEN}Starting Server${NC}"
+echo "==================================================================="
+echo ""
+npm start
 
-
-echo "If you can read this, something probably failed..."
+echo ""
+echo "==================================================================="
+echo "${RED}If you can read this, something probably failed...${NC}"
+echo "==================================================================="
+echo ""
