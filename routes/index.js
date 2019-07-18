4057603920e511558a8b72870cd8d37ea6c8c31f
Shannon Darroch <shannondarroch@Shannons-MacBook-Pro.local>
2019-07-18 14:03:03 -0400

    Added video support

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,8 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 //CHANGE THESE KEYS FOR S3 ACCESS
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: '',
+  secretAccessKey: '',
   region: 'us-east-2'
 });
 
